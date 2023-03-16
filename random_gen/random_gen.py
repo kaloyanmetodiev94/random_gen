70b4c69ab2fab1771e4dad1b4ef91827a40fe330
Kaloyan Metodiev <kaloyan.metodiev@endurosat.com>
2023-03-16 13:19:13 +0200

    Initial port to new repo

---
+++ random_gen/random_gen.py
@@ -0,0 +1,77 @@
+import random
+from collections import defaultdict #This is used to gather information for the example
+import warnings
+
+class RandomGen:
+	def __init__(self, random_nums: list, probabilities: list):
+		"""
+		Initializes the RandomGenerator class with a list of values and their corresponding probabilities.
+		
+		Args:
+		- values (list): A list of values to choose from.
+		- probabilities (list): A list of probabilities for each value.
+
+        Raises:
+        TypeError: If values or probabilities are not numeric.
+        ValueError: If the sum of probabilities is greater than 1.0.
+
+		"""
+		self._random_nums = random_nums
+		self._probabilities = probabilities
+
+		# Since it is not specified whether the inputted values need to be numeric or not, this is just a warning for now 
+		if not all(isinstance(x, (int, float)) for x in self._random_nums):
+			warnings.warn("Input values should be numeric")
+			
+		if not all(isinstance(x, (int, float)) for x in self._probabilities): 
+			raise TypeError("Probabilities must be numeric")
+		
+		# Check that the lengths of the two input lists match
+		if len(self._random_nums) != len(self._probabilities):
+			raise ValueError("The length of the 'values' list must match the length of the 'probabilities' list.")
+
+		self._len=len(self._random_nums) # Save the length of both sequences, so that it is not recalculated anymore
+		
+		# Check that the probabilities add up to 1
+		if abs(sum(self._probabilities) - 1.0) > 0.00001:
+			raise ValueError("The probabilities must add up to exactly 1.")
+
+	
+	def next_num(self):
+		"""
+		Returns a randomly selected value from the 'values' list based on the corresponding probabilities.
+		
+		Returns:
+		- A randomly selected value from the 'values' list.
+		"""
+		rand_val = random.random() # Generate a random number between 0 and 1
+		cum_prob = 0.0 # Initialize the cumulative probability
+		
+		# Iterate over the values and their corresponding probabilities
+		for i in range(self._len):
+			cum_prob += self._probabilities[i] # Add the current probability to the cumulative probability
+			
+			# If the cumulative probability is greater than the random number, return the corresponding value
+			if rand_val < cum_prob:
+				return self._random_nums[i]
+		
+		# If we get here, something went wrong
+		raise ValueError("The probabilities did not add up to 1.")
+		
+if __name__=='__main__': # We reproduce the example here
+	probabilities = [0.01, 0.3, 0.58, 0.1, 0.01]
+	random_nums = [-1, 0, 1, 2, 3]
+	
+	rg = RandomGen(random_nums, probabilities)
+	
+	# Run the next_num() function 100 times and collect the results to reproduce the example (of course not exactly)
+	example_range=100
+	example_values={}
+	example_values=defaultdict(lambda: 0, example_values) # If key is non-existent, initialize with 0
+	for i in range(example_range):
+			example_values[rg.next_num()]+=1        
+	
+	#print out the result
+	for k in random_nums:
+		print(k,':',example_values[k],'times')
+  
---
+++ random_gen/tests/tests.py
@@ -0,0 +1,66 @@
+import unittest
+from random_gen import RandomGen
+
+class TestRandomGenMain(unittest.TestCase):
+
+	def __init__(self, *args, **kwargs):
+		self._typical_runs=10_000_000 # Asssume a use case would call the next_num 10_000_000 times for some of the tests
+		super(TestRandomGenMain, self).__init__(*args, **kwargs)
+
+	def test_init(self):
+		# Test that the constructor raises a ValueError when the input lists have different lengths
+		with self.assertRaises(ValueError):
+			RandomGen([1, 2, 3], [0.1, 0.2, 0.3, 0.4])
+		
+		# Test that the constructor raises a ValueError when the probabilities less than 1
+		with self.assertRaises(ValueError):
+			RandomGen([1, 2, 3], [0.1, 0.2, 0.3])
+
+		# Test that the class raises an error if any probability is greater than 1
+		with self.assertRaises(ValueError):
+			rg = RandomGen([1, 2, 3], [0.5, 0.25, 1.1])
+	
+	def test_next_num_distribution(self):
+		# Test that the next_num function returns the correct values based on the input probabilities
+		rg = RandomGen([1, 2, 3], [0.1, 0.2, 0.7])
+		vals_dict={1:0,2:0,3:0}
+		total_values=self._typical_runs//100 
+		for _ in range(total_values):
+			vals_dict[rg.next_num()]+=1
+		self.assertAlmostEqual(vals_dict[1]/total_values, 0.1, delta=0.01)
+		self.assertAlmostEqual(vals_dict[2]/total_values, 0.2, delta=0.01)
+		self.assertAlmostEqual(vals_dict[3]/total_values, 0.7, delta=0.01)
+
+	def test_next_num_input_values(self):
+		# Test that the next_num function returns a value from the input list
+		rg = RandomGen([1, 2, 3], [0.3, 0.4, 0.3])
+		for _ in range(self._typical_runs//10): 
+			self.assertIn(rg.next_num(), [1, 2, 3])
+		
+
+	def test_next_num_probability_equal_1(self):
+		# Test that the next_num function raises a ValueError when the probabilities do not add up to 1
+		rg = RandomGen([1, 2, 3], [0.8, 0.1, 0.1])
+		with self.assertRaises(ValueError):
+			rg._probabilities[0]=0.2 # Intentionally change probability to pass the check in the constructor
+			for _ in range(self._typical_runs): # There is a probability of 60% the next_num() fails with the "adjusted" probabilities and we call it {self._typical_runs} times.
+				rg.next_num()
+
+class TestRandomGenEdgeCases(unittest.TestCase):
+	def test_empty_input_lists(self):
+	    # Test that the class raises an error if both input lists are empty
+	    with self.assertRaises(ValueError):
+	        rg = RandomGen([], [])
+
+	def test_non_numeric_input_values(self):
+	    # Test that the class raises an error if any input value is non-numeric
+	    with self.assertWarns(Warning):
+	        rg = RandomGen(["a", "b", "c"], [0.3, 0.5, 0.2])
+
+	def test_non_numeric_probabilities(self):
+	    # Test that the class raises an error if any of the inputted is non-numeric even if it is convertible to numeric
+	    with self.assertRaises(TypeError):
+	        rg = RandomGen([1, 2, 3], [0.3, "0.5", 0.2])
+
+if __name__ == '__main__':
+	unittest.main()
